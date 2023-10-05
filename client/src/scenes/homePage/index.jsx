1aed76489bd022f2f8ffc3ae8959ff274a109ae5
AmulyaMurahari <37552813+AmulyaMurahari@users.noreply.github.com>
2023-10-05 01:02:23 -0400

    commits

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,11 +1,13 @@
-import Navbar from "../navbar";
+import Navbar from "../../scenes/navbar";
+import { Box } from "@mui/material";
+
 
 const HomePage = () => {
     return(
         <Box>
             <Navbar />
         </Box>
-    )
-}
+    );
+};
 
 export default HomePage;
